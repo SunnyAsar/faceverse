f0bfbedc257098d6e2282139112d2cabd7220065
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-21 17:17:01 -0500

    [update] relation names for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,13 +7,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, foreign_key: :author_id
-  has_many :friends_requested, class_name: "FriendRequest", foreign_key: :sender_id
-  has_many :friends_requesting, class_name: "FriendRequest", foreign_key: :receiver_id
 
+  has_many :sent_requests, class_name: 'FriendRequest', foreign_key: :sender_id
+  has_many :friends_requested, through: :sent_requests, source: :receiver
 
-  has_many :sent_requests, through: :friends_requested, source: :receiver
-
-  has_many :received_requests, through: :friends_requesting, source: :sender
-  
-
+  has_many :received_requests, class_name: 'FriendRequest', foreign_key: :receiver_id
+  has_many :friends_requesting, through: :received_requests, source: :sender
 end
