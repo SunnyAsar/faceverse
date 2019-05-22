5cfaf6301675a1e53b4102f683b841f8f4a4cf76
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-22 18:33:44 -0500

    [add] friends posts in post index controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = current_user.friends.reduce()posts
+    @posts = current_user.posts
+    current_user.friends.each do |friend|
+      @posts += friend.post
+    end
   end
 end
