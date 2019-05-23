882862ec0bffd0f2d7c0da954bc1e432fd119fbc
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-23 18:51:34 -0500

    [add] first and last name to users

--- factories.rb
+++ factories.rb
@@ -2,9 +2,11 @@ require  'faker'
 
 FactoryBot.define do
   factory :user do
-    email {Faker::Internet.email}
-    password {'foobar'}
-    password_confirmation {'foobar'}
+    email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    password { 'foobar' }
+    password_confirmation { 'foobar' }
   end
 
   factory :post do
@@ -12,7 +14,7 @@ FactoryBot.define do
     association :author, factory: :user
   end
 
-  factory :friendship do 
+  factory :friendship do
     association :user, factory: :user
     association :friend, factory: :user
   end
