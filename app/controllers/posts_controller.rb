972806856dc3c4e6e1e48ec8212dacb65996b8d3
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-27 17:48:02 -0500

    [add] index and create methods for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
   def index
-    ids = current_user.friends.pluck(:id) << current_user.id
-    @posts = Post.where(author_id: ids)
+    @posts = Post.feed_for(current_user)
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'Post created'
+    else
+      flash[:alert] = 'Unable to create post, try to add content before post'
+    end
+    redirect_to root_url
+  end
+
+  def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users, controllers: { registrations: 'registrations' }
+  resources :posts, only: %i[index create destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
