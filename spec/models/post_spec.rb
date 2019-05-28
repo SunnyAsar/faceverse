84e1487c0d0ca0faeb9d3b2fa67a2ae40b0cacef
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-28 12:46:45 -0500

    [add] factory bot support on rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,25 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:user) {User.create!(first_name:'s',last_name:'j', email: 'jay2@he.com',password:'foobar',password_confirmation:'foobar')}
+  let(:user) { create(:user) }
 
-  it "expects post to be valid" do
-    post = Post.create!(content: "the test post", author_id: user.id).save
+  it 'expects post to be valid' do
+    post = Post.create!(content: 'the test post', author_id: user.id).save
     expect(post).to eq(true)
   end
 
   it 'expects author to be invalid' do
-    post = Post.new(content: "the test post").save
+    post = Post.new(content: 'the test post').save
     expect(post).to eq(false)
   end
 
   it 'expects content to be invalid' do
-    post = Post.new(author_id:user.id).save
+    post = Post.new(author_id: user.id).save
     expect(post).not_to eq(true)
   end
 
-  it 'expects post to be stored' do 
-    post = Post.new(content: "the test post", author_id: user.id).save
+  it 'expects post to be stored' do
+    post = Post.new(content: 'the test post', author_id: user.id).save
     expect(Post.count).to eq(1)
   end
 
@@ -27,5 +29,4 @@ RSpec.describe Post, type: :model do
     it { should belong_to(:author) }
     it { should have_many(:comments) }
   end
-
 end
