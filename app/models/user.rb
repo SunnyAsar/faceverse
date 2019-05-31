8fdee8fe30eee411b869144406fa40b7067471c6
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-31 16:20:17 -0500

    [add] facebook auth details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'byebug'
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -6,8 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
 
   has_many :posts, foreign_key: :author_id, dependent: :destroy
 
@@ -91,23 +91,11 @@ class User < ApplicationRecord
     end
   end
 
-
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.first_name = auth.info.name   # assuming the user model has a name
-      # user.last_name = auth.info.last_name   # assuming the user model has a name
-      # user.last_name = auth.info.last_name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-
+      user.first_name = auth.info.name
     end
   end
-
-  
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  context 'field validations' do
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-  end
-
   context 'testing Associations' do
     it { should have_many(:posts).dependent(:destroy)}
     it { should have_many(:comments).dependent(:destroy) }
