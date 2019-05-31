ee43e96e4dbf8df52e7be56f7ceccd2c2d197906
Sunny <ring2rings93@gmail.com>
2019-05-31 21:45:03 +0100

    omniauth with facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       # debugger
     if @user.persisted?
-      sign_in @user #this will throw if @user is not activated
+      sign_in_and_redirect @user #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
