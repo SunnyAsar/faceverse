a1847f9b3d85039d82d9311598e1e3416332120b
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-06-06 12:31:47 -0500

    [add] factories for testing users posts and likes

--- factories.rb
+++ factories.rb
@@ -1,20 +1,12 @@
 require 'faker'
 
 FactoryBot.define do
-  factory :user, aliases: %i[author commenter friend sender receiver] do
+  factory :user, aliases: %i[author commenter friend sender receiver liker] do
     email { Faker::Internet.email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     password { 'foobar' }
     password_confirmation { 'foobar' }
-
-    factory :user_with_friends do
-      transient { count { 3 } }
-
-      after :create do |user, vars|
-        create_list(:friendship, vars.count, user: user)
-      end
-    end
 
     factory :user_with_relations do
       transient do
@@ -29,6 +21,18 @@ FactoryBot.define do
         create_list(:friend_request, vars.received_friend_requests_count, receiver: user)
       end
     end
+
+    factory :user_with_posts_and_likes do
+      transient do
+        posts_count { 3 }
+        liked_count { 3 }
+      end
+
+      after :create do |user, vars|
+        create_list(:post_with_likes, vars.posts_count, author: user)
+        create_list(:like, vars.liked_count, :for_post, liker: user)
+      end
+    end
   end
 
   factory :post do
@@ -37,6 +41,14 @@ FactoryBot.define do
 
     factory :invalid_post do
       content { '' }
+    end
+
+    factory :post_with_likes do
+      transient { count { 3 } }
+
+      after :create do |post, vars|
+        create_list(:like, vars.count, :for_post, likeable: post)
+      end
     end
   end
 
@@ -61,6 +73,7 @@ FactoryBot.define do
   end
 
   factory :like do
+    liker
     for_post
 
     trait :for_post do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,53 +18,68 @@ RSpec.describe User, type: :model do
   end
 
   describe 'public instance methods' do
-    let(:user) do
-      create(:user_with_relations, friends_count: 5,
-                                   sent_friend_requests_count: 2,
-                                   received_friend_requests_count: 3)
+    let(:user) { create(:user) }
+
+    it '#full_name returns a concat of first_name and last_name' do
+      expect(user.full_name).to eq([user.first_name, user.last_name].join(' '))
     end
 
-    it '#friends returns 5 users' do
-      expect(user.friends.count).to eq(5)
+    context 'for relationships' do
+      let(:user) do
+        create(:user_with_relations, friends_count: 5,
+                                     sent_friend_requests_count: 2,
+                                     received_friend_requests_count: 3)
+      end
+      let(:unrelated_user) { create(:user) }
+      let(:friend_user) { user.friends.first }
+      let(:requested_user) { user.friends_requested.first }
+      let(:requesting_user) { user.friends_requesting.first }
+
+      it '#friends returns 5 users' do
+        expect(user.friends.count).to eq(5)
+      end
+
+      it '#friend? returns True for a friend id' do
+        expect(user.friend?(friend_user.id)).to be true
+      end
+
+      it '#friend? returns False if is not a friend id' do
+        expect(user.friend?(unrelated_user.id)).to be false
+      end
+
+      it '#friend_requested? returns True for a requested friend id' do
+        expect(user.friend_requested?(requested_user.id)).to be true
+      end
+
+      it '#friend_requested? returns False if is not a requested friend id' do
+        expect(user.friend_requested?(unrelated_user.id)).to be false
+      end
+
+      it '#friend_requesting? returns True for a requesting friend id' do
+        expect(user.friend_requesting?(requesting_user.id)).to be true
+      end
+
+      it '#friend_requesting? returns False if is not a requesting friend id' do
+        expect(user.friend_requesting?(unrelated_user.id)).to be false
+      end
+
+      it '#request_from returns the friend_request instance that relate the users' do
+        expect(user.request_from(requesting_user.id)).to be_a FriendRequest
+      end
+
+      it '#friend_requests returns users requesting friendship' do
+        expect(user.friend_requests).to eq user.friends_requesting
+      end
     end
 
-    it '#friend? returns True for a friend id' do
-      friend = user.friends.first
-      expect(user.friend?(friend.id)).to be true
-    end
+    context 'for post and it likes' do
+      let(:user) do
+        create(:user_with_posts_and_likes)
+      end
 
-    it '#friend? returns False if is not a friend id' do
-      not_friend = user.friends_requesting.first
-      expect(user.friend?(not_friend.id)).to be false
-    end
-
-    it '#friend_requested? returns True for a requested friend id' do
-      requested = user.friends_requested.first
-      expect(user.friend_requested?(requested.id)).to be true
-    end
-
-    it '#friend_requested? returns False if is not a requested friend id' do
-      not_requested = create(:user)
-      expect(user.friend_requested?(not_requested.id)).to be false
-    end
-
-    it '#friend_requesting? returns True for a requesting friend id' do
-      requesting = user.friends_requesting.first
-      expect(user.friend_requesting?(requesting.id)).to be true
-    end
-
-    it '#friend_requesting? returns False if is not a requesting friend id' do
-      not_requesting = create(:user)
-      expect(user.friend_requesting?(not_requesting.id)).to be false
-    end
-
-    it '#request_from returns the friend_request instance that relate the users' do
-      requesting = user.friends_requesting.first
-      expect(user.request_from(requesting.id)).to be_a FriendRequest
-    end
-
-    it '#friend_requests returns users requesting friendship' do
-      expect(user.friend_requests).to eq user.friends_requesting
+      it 'user have 3 posts' do
+        expect(user.posts.count).to eq(3)
+      end
     end
   end
 end
