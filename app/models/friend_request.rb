07d0c3f994a33dfafda07a357f028fe2329d4516
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-06-07 16:17:34 -0500

    [add] last models tests

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -7,8 +7,4 @@ class FriendRequest < ApplicationRecord
   scope :related, ->(user_id, friend_id) { where('sender_id = ? AND receiver_id = ?', user_id, friend_id)
                                             .or(FriendRequest.where('sender_id = ? AND receiver_id = ?', friend_id, user_id)) }
 
-  private
-  def order_params
-    self.sender_id, self.receiver_id = receiver_id, sender_id if receiver_id < sender_id
-  end
 end
--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FriendRequest, type: :model do
-  
   context 'Association' do
     it { should belong_to(:sender) }
     it { should belong_to(:receiver) }
