c20248222993760ca9b3427e370087a0018c863b
Sunny <ring2rings93@gmail.com>
2019-06-07 19:06:58 +0100

    update and delete controller test for post, index and show for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  before(:each) do 
+    @user = create(:user)
+    sign_in @user
+  end
+
   describe "GET #index" do
     it "returns http success" do
-      # @user = create(:user)
-      sign_in :user
       get :index
       expect(response).to have_http_status(:success)
+      # expect(assigns(:users)).to eq([@user])
+    end
+  end
+
+  describe 'GET #show' do
+    it 'return the details of a user' do
+      get :show, params: {id: @user}
+      expect(response.success?)
+      expect(assigns(:user)).to eql(@user)
+      expect(response).to render_template :show
     end
   end
 
