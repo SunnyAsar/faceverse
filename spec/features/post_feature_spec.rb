a3479c2878f41a7ce3cc276edd8c8f3858e895f5
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-06-11 12:11:58 -0500

    [update] selenium chrome driver

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -6,34 +6,34 @@ RSpec.describe "Testing post", type: :feature do
     @post = create(:post, author: @user)
   end
 
-
   it 'creates a post from valid content' do
     visit root_path
     expect(page).to have_content 'News Feed'
-    within('#new_post') do 
+    within('#new_post') do
       fill_in 'post_content', with: 'hello new post here'
     end
     click_button 'Create Post'
     expect(page).to have_content 'Post created'
   end
 
-  it 'fails to create post with invalid content' do 
+  it 'fails to create post with invalid content' do
     visit root_path
-    within('#new_post') do 
+    within('#new_post') do
       fill_in 'post_content', with: ' '
     end
     click_button 'Create Post'
     expect(page).to have_content "Content can't be blank"
   end
 
-
-  it 'deletes a post' do
-    visit root_path
-    expect(page).to have_link('Destroy', href:"/posts/#{@post.id}")
-    accept_alert do
-      click_link('Destroy', href:"/posts/#{@post.id}")
+  # scenario js: true do
+    it 'deletes a post' do
+      visit root_path
+      expect(page).to have_link('Destroy', href: post_path(@post))
+      accept_alert do
+        click_link('Destroy', href: post_path(@post))
+      end
+      expect(page).to have_current_path(root_path)
     end
-    expect(page).to have_current_path(root_path)
-  end
+  # end
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 # include Warden::Test::Helpers
 require 'spec_helper'
-require 'capybara/rspec'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
