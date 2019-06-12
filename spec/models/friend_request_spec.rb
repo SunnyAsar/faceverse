8ac4f256974591f33e10ab42a15dbc8130e8e2de
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-06-11 22:47:38 -0500

    [add] friend request validation callback test

--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe FriendRequest, type: :model do
     it { should belong_to(:sender) }
     it { should belong_to(:receiver) }
   end
+
+  context 'Callbacks' do
+    let(:friend_request) do
+      user = create(:user)
+      friend = create(:user)
+      create(:friendship, user: user, friend: friend)
+      build(:friend_request, sender: user, receiver: friend)
+    end
+    it '#validate_frienship invalidate friend request for users that are already friends' do
+      expect(friend_request).to_not be_valid
+    end
+  end
 end
